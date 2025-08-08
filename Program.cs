df39fec0580b67b0c1bcd7e91c401a1105a34284
HappyNZ95 <htewini@gmail.com>
2025-08-08 13:52:47 +1200

    getUserInput()
    created the getUserInput() function.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace MarkdownGenerator
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = getUserInput("Title: ");
+            string tags = getUserInput("Tags: ", true);
+            List<String> tagList = new List<String>();
+        }
+
+        static string? getUserInput(string prompt, bool allowEmpty = false)
+        {
+            bool waitingForUserInput = true;
+            while (waitingForUserInput)
+            {
+                Console.WriteLine($"{prompt}");
+
+                string? userInput = Console.ReadLine();
+                if (userInput != null && userInput.Trim() != "")
+                {
+                    return userInput.Trim();
+                }
+                else if (allowEmpty == true)
+                {
+                    return null;
+                }
+
+            }
+            return null;
+
+
+
+
+        }
+    }
+}
